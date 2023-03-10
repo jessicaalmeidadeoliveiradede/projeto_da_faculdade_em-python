72429f6cad2ecff1f5d97f76854cf7da29e1a89e
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2023-03-10 20:57:16 -0300

    projeto python

---
+++ primeiroprojeto.py
@@ -0,0 +1,6 @@
+s=int (input("digite os seu salario"))
+inss=int (input("digite o valor do seu inss"))
+s:s+inss/18
+
+print("digite a soma entre vo seu salario junto a inss") 
+print("o valor do disconto e")
